5ade5cde0032f8212f28ac20236ec6dfb71defc6
gentlecolts <zangoose@new-host-2.home>
2016-09-14 00:01:20 -0400

    whoopse, still getting used to command line git, now things should be good

---
+++ engine/core/event.h
@@ -0,0 +1,10 @@
+namespace MG{
+	class event{
+	protected:
+		virtual void quit();//called when main engine object is destroyed
+		virtual void keyDown(char key);
+		virtual void keyUp(char key);
+	public:
+		event();
+	};
+}
